1eecef047aaacdd8af960748309a68336102126a
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-04-09 00:11:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* <h2 className="bg-orange-500">Hello digitools</h2> */}
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
